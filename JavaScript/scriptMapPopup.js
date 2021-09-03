ee285342a6559d41c01c3e44542ed2ba76f3f61d
Fernando Fernandez <fermen1305@gmail.com>
2021-09-03 09:16:44 -0500

    Regiones texto añadido

--- JavaScript/scriptMapPopup.js
+++
@@ -1,5 +0,0 @@
-function changeText(){
-    document.getElementById("titlePopup").innerHTML = "Amazonas";
-    document.getElementById("textPopup").innerHTML = "El Departamento de Amazonas es sinónimo de alegría, paz y fraternidad, siendo considerada como el lugar del planeta con la más fascinante e insospechada vitrina de recursos naturales.";
-    document.getElementById("textPopup2").innerHTML = "El elemento principal es el Río Amazonas, como fuente de vida y centro en torno al cual giran gran parte de las actividades del Departamento. Está sobre un fondo verde que representa la gran capa vegetal de la selva virgen, llena de maravillas tanto en la flora como en la fauna.";
-}
